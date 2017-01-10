d56b85e8d5b4a7c4f68dab3b524e4537f7f43b8f
Dan Cohen <dcohen@usccreditunion.org>
2017-01-10 15:06:59 -0800

    Reference tarball

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'description': 'slack_logging_handler',
     'author': 'Dan Cohen',
     'url': 'www.github.com/danie1cohen/slack_logging_handler',
-    'download_url': 'https://www.github.com/danie1cohen/slack_logging_handler.git',
+    'download_url': 'https://github.com/danie1cohen/slack_logging_handler/tarball/0.1.10',
     'author_email': 'daniel.o.cohen@gmail.com',
     'version': '0.1.10',
     'install_requires': [],
